d9c94440243fb4a4213ffc091a66f1a3ec540af6
Peter Große <pegro@fem-net.de>
2018-09-10 22:53:23 +0200

    clean up unused files

--- ext_tables.php
+++ ext_tables.php
@@ -6,26 +6,10 @@ if (TYPO3_MODE === 'BE') {
 	$GLOBALS['TCA']['tt_content']['columns']['image_effects']['config']['items'][10] = array('LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang.xlf:image_effects.I.10', 31);
 	$GLOBALS['TCA']['tt_content']['columns']['image_effects']['config']['items'][11] = array('LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang.xlf:image_effects.I.11', 32);
 	$GLOBALS['TCA']['tt_content']['columns']['image_effects']['config']['items'][12] = array('LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang.xlf:image_effects.I.12', 33);
-	
-	// unserializing the extension configuration
-	$_EXTCONF = unserialize($_EXTCONF);
-/*
-	if ($_EXTCONF['showTestModule']) {
-		\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-			'ImagickImgTeam.' . $_EXTKEY,
-			'system',
-			'imagickTest',
-			'top',
-			array(
-				'ImagickInfo' => 'index',
-				'ImagickTests' => 'index,read,write,convert,scale,combine',
-			),
-			array(
-				'access' => 'user,group',
-				'icon' => 'EXT:' . $_EXTKEY . '/Resources/Public/Backend/Icons/ModuleIcon.png',
-				'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xlf',
-			)
-		);
-	}
-*/
+
+    // Hooks
+    // Show warning in About module if PHP extension Imagick is not loaded
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_befunc.php']['displayWarningMessages'][$_EXTKEY] = \ImagickImgTeam\Imagickimg\WarningMessagePostProcessor::class;
+    // Show warning in Reports module if PHP extension Imagick is not loaded
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers'][$_EXTKEY][] = \ImagickImgTeam\Imagickimg\RequirementsCheckUtility::class;
 }
